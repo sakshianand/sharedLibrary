426004044c1e86cc6816122c515f78a387f8daaa
sakshianand <anandsakshi2016@gmail.com>
2019-09-06 17:12:46 +0530

    Update github_connector.groovy

--- vars/github_connector.groovy
+++ vars/github_connector.groovy
@@ -1,6 +1,13 @@
 def call(){
-  def response = httpRequest "https://api.github.com/repositories"
-  println('Status:')
+  httpRequest acceptType: 'APPLICATION_JSON', authentication: 'connectors', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: '''{
+                "name": "chaitanya",
+                  "description": "This is your first repository",
+                  "homepage": "https://github.com",
+                  "private": false,
+                  "has_issues": true,
+                  "has_projects": true,
+                  "has_wiki": true
+                }''', responseHandle: 'NONE', url: 'https://api.github.com/user/repos'
 }
 
 
