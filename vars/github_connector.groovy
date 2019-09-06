52f3f902a8c832828459d932f259b5bdd4ee8b5d
sakshianand <anandsakshi2016@gmail.com>
2019-09-06 17:59:49 +0530

    Update github_connector.groovy

--- vars/github_connector.groovy
+++ vars/github_connector.groovy
@@ -1,6 +1,6 @@
 def call(){
   httpRequest acceptType: 'APPLICATION_JSON', authentication: 'connectors', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: '''{
-                "name": "chaitanya",
+                "name": "vinu",
                   "description": "This is your first repository",
                   "homepage": "https://github.com",
                   "private": false,
