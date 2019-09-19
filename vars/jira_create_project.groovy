0595aea03efa2b3b80d34e79c1de2dae60125459
sakshianand <anandsakshi2016@gmail.com>
2019-09-19 20:45:12 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -1,22 +1,17 @@
 // def request = libraryResource 'data.json'
 import groovy.json.JsonSlurper 
 
-
+@NonCPS
+def getJSONData(String json) { 
+   def jsonSlurper = new JsonSlurper() 
+   def resultJson = jsonSlurper.parseText(request)
+   resultJson.each {
+      print “${it.name}”
+   }
+}
 def call(){
-def request = libraryResource 'data.json'
-def jsonSlurper = new JsonSlurper()
-def object = jsonSlurper.parseText(request)
-httpRequest authentication: 'jira_password', 
-    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
-                    [maskValue: false, name: 'Accept', value: 'application/json']], 
-    httpMode: 'POST', requestBody: '''{
-    "key": "PIZ",
-    "name": "${object.name}",
-    "projectTypeKey": "business",
-    "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-project-management",
-    "description": "Example Project description",
-    "lead": "ashnim",
-    "assigneeType": "PROJECT_LEAD"
-}''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project'
+ def request = libraryResource 'data.json'
+ getJSONData(request)
+
 println object.name
 }
