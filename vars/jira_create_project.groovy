f4ea3d67a28b57c4039bdd0751ff233366952465
sakshianand <anandsakshi2016@gmail.com>
2019-09-19 20:49:06 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -3,15 +3,15 @@ import groovy.json.JsonSlurper
 
 @NonCPS
 def getJSONData(String json) { 
-   def jsonSlurper = new JsonSlurper() 
-   def resultJson = jsonSlurper.parseText(request)
-   resultJson.each {
+   json.each {
       print “${it.name}”
    }
 }
 def call(){
  def request = libraryResource 'data.json'
- getJSONData(request)
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ getJSONData(resultJson)
 
 println object.name
 }
