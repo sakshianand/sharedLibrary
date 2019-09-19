e8b80b39f86ba377b74b86019be1c28f22db3835
sakshianand <anandsakshi2016@gmail.com>
2019-09-19 17:31:57 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -2,7 +2,7 @@
 
 def call(){
 def request = libraryResource 'data.json'
-println request
+    println request.name
 httpRequest authentication: 'jira_password', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: '''{
     "key": "EXAM",
     "name": "example2",
