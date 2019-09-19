c43d332850275076e524635b8b7b3e9deeeae00b
sakshianand <anandsakshi2016@gmail.com>
2019-09-19 15:46:05 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -1,9 +1,9 @@
 httpRequest authentication: 'jira_password', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: '''{
-    "key": "EX",
-    "name": "Example",
+    "key": "PRA",
+    "name": "prakhar",
     "projectTypeKey": "business",
     "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-project-management",
     "description": "Example Project description",
-    "lead": "ashnim"
+    "lead": "ashnim",
     "assigneeType": "PROJECT_LEAD"
 }''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project'
