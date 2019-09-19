0920c384a00dad6381dd473ede6bfab6cb0bae32
sakshianand <anandsakshi2016@gmail.com>
2019-09-19 15:04:22 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -1,1 +1,12 @@
 
+def call(){
+  httpRequest authentication: 'jira_password', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: '''{
+    "key": "EX",
+    "name": "Example",
+    "projectTypeKey": "business",
+    "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-project-management",
+    "description": "Example Project description",
+    "lead": "ashnim"
+    "assigneeType": "PROJECT_LEAD"
+}''', responseHandle: 'NONE', url: 'https://github.com/sakshianand/sharedLibrary.git'
+}
