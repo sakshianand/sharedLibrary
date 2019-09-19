731f51676551cfa9020c41eac31335313531dcf4
sakshianand <anandsakshi2016@gmail.com>
2019-09-20 00:05:01 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -8,18 +8,16 @@ x(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def projectName = '"'+resultJson.name+'"'
- httpRequest authentication: 'jira_password', 
-    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
-                    [maskValue: false, name: 'Accept', value: 'application/json']], 
-    httpMode: 'POST', requestBody: '''{
+println '''{
     "key": "JUP",
-    "name": $projectName,
+    "name": ${projectName},
     "projectTypeKey": "business",
     "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-project-management",
     "description": "Example Project description",
     "lead": "ashnim",
     "assigneeType": "PROJECT_LEAD"
-}''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project'
+}'''
+
  
 }
 def call(){
