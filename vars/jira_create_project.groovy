96c7a473a222432d0aa084a2778f0f4899310b58
sakshianand <anandsakshi2016@gmail.com>
2019-09-20 00:03:22 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -13,7 +13,7 @@ def projectName = '"'+resultJson.name+'"'
                     [maskValue: false, name: 'Accept', value: 'application/json']], 
     httpMode: 'POST', requestBody: '''{
     "key": "MAR",
-    "name": "mars",
+    "name": $projectName,
     "projectTypeKey": "business",
     "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-project-management",
     "description": "Example Project description",
