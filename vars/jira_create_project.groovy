1677f8526fab36126f5fbe0ace700c29013cb1db
sakshianand <anandsakshi2016@gmail.com>
2019-09-23 09:11:09 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -13,7 +13,18 @@ def projLength = resultJson.name.size()
  if(length>=3){
   key=resultJson.name.substring(0, Math.min(projLength, length)).toUpperCase();
  }
- println key
+ httpRequest authentication: 'jira_password', 
+    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']], 
+    httpMode: 'POST', requestBody: """{
+    "key": ${key},
+    "name": ${projectName},
+    "projectTypeKey": "business",
+    "projectTemplateKey": "com.atlassian.jira-core-project-templates:jira-core-project-management",
+    "description": "Example Project description",
+    "lead": "ashnim",
+    "assigneeType": "PROJECT_LEAD"
+}""", responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project'
 
 
 
