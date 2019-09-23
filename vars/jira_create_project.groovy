6c02071b5e2a0db93438654bc020eb225622da1b
sakshianand <anandsakshi2016@gmail.com>
2019-09-23 09:44:58 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -10,15 +10,23 @@ def resultJson = jsonSlurper.parseText(data)
 def projectName = '"'+resultJson.name+'"'
 def length = 3
 def projLength = resultJson.name.size()
- if(length>=3){
+ if(projLength>=3){
   key=resultJson.name.substring(0, Math.min(projLength, length)).toUpperCase();
  }
+ else {
+   def appendStr = "";
+   int currentLength=length;
+   int len = currentLength%projLength;
+   int repeat = currentLength/projLength;
+   for (int i=0;i<repeat;i++) {
+    appendStr = appendStr + projectName;
+   }
+   appendStr=appendStr+name.substring(0, Math.min(projectName.length(), len));
+   key=appendStr.toUpperCase();
+ }
  def projKey = '"'+key+'"'
-
-httpRequest authentication: 'jira_password', 
-    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
-                    [maskValue: false, name: 'Accept', value: 'application/json']], 
-    httpMode: 'POST', requestBody: """{
+ 
+ println """{
     "key": ${projKey},
     "name": ${projectName},
     "projectTypeKey": "business",
@@ -26,7 +34,9 @@ httpRequest authentication: 'jira_password',
     "description": "Example Project description",
     "lead": "ashnim",
     "assigneeType": "PROJECT_LEAD"
-}""", responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project'
+}"""
+
+
 
 
  
