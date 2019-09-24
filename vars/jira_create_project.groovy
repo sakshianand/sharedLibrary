962829bb6502c89723d15a0a3eeee4b0091fbff2
sakshianand <anandsakshi2016@gmail.com>
2019-09-24 15:58:12 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -2,6 +2,38 @@
 import groovy.json.JsonSlurper 
 
 
+createIssues(String projKey){
+ httpRequest authentication: 'jira_password', 
+    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']], 
+    httpMode: 'POST', requestBody: """
+    {
+        "issueUpdates": [
+        {
+        "update":{},
+        "fields": {
+        "project":
+        {
+         "key": "SAK"
+        },
+        "summary": "this is summary 1",
+        "description": "this is description",
+        "issuetype": {
+        "name": "Task"
+        },
+        "assignee": {
+        "name": "ashnim"
+        },
+        "reporter": {
+        "name": "ashnim"
+        }
+        }
+        }
+        ]
+} """, responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/bulk'
+}
+
+
 
 @NonCPS
 createProject(String data){
@@ -39,7 +71,7 @@ httpRequest authentication: 'jira_password',
     "assigneeType": "PROJECT_LEAD"
 }""", responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/project'
 
-
+createIssues(projKey)
 
 
  
