e801ea17b729bdc5b13e475a902f0fa93d04d363
sakshianand <anandsakshi2016@gmail.com>
2019-09-24 16:22:25 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -3,10 +3,7 @@ import groovy.json.JsonSlurper
 
 
 createIssues(String projKey){
- httpRequest authentication: 'jira_password', 
-    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
-                    [maskValue: false, name: 'Accept', value: 'application/json']], 
-    httpMode: 'POST', requestBody: """
+ println  """
     {
         "issueUpdates": [
         {
@@ -30,7 +27,7 @@ createIssues(String projKey){
         }
         }
         ]
-} """, responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/bulk'
+} """
 }
 
 
