591bd45072d50894b7a4266953b6a65358719cf8
sakshianand <anandsakshi2016@gmail.com>
2019-09-30 00:09:54 +0530

    Update jira_create_project.groovy

--- vars/jira_create_project.groovy
+++ vars/jira_create_project.groovy
@@ -25,18 +25,30 @@ def projLength = resultJson.name.size()
  def projKey = '"'+key+'"'
  
  httpRequest authentication: 'Jira_Cred', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: '''{
-    "fields": {
-       "project":
-       {
-          "key": "SAK"
-       },
-       "summary": "this is summary 1",
-       "description": "this is description",
-       "issuetype": {
-          "name": "Task"
-       }
-   }
-}''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/'
+    "issueUpdates": [
+{
+"update":{},
+"fields": {
+"project":
+{
+ "key": "SAK"
+},
+"summary": "this is summary 1",
+"description": "this is description",
+"issuetype": {
+	"name": "Task"
+},
+"assignee": {
+	"name": "ashnim"
+},
+"reporter": {
+	"name": "ashnim"
+}
+
+}
+}
+]
+}''', responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/bulk'
 
  
 
