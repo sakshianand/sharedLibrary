f729592bee3045229e504ae0bc2364dc5ddd386b
sakshianand <anandsakshi2016@gmail.com>
2019-09-30 12:29:00 +0530

    Update jira_create_issues.groovy

--- vars/jira_create_issues.groovy
+++ vars/jira_create_issues.groovy
@@ -47,6 +47,33 @@ println """ "issueUpdates": [
 }
 }
 ] """
+	
+
+ httpRequest authentication: 'jira_password', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: """{
+    "issueUpdates": [
+{
+"update":{},
+"fields": {
+"project":
+{
+ "key": ${projKey}
+},
+"summary": "this is summary 1",
+"description": "this is description",
+"issuetype": {
+	"name": "Task"
+},
+"assignee": {
+	"name": "ashnim"
+},
+"reporter": {
+	"name": "ashnim"
+}
+
+}
+}
+]
+}""", responseHandle: 'NONE', url: 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/issue/bulk'
 
  
 
