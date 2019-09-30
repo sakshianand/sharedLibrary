b0b22c5764709b7605efd286ec1f8b2959751d47
sakshianand <anandsakshi2016@gmail.com>
2019-09-30 12:06:05 +0530

    Update jira_create_issues.groovy

--- vars/jira_create_issues.groovy
+++ vars/jira_create_issues.groovy
@@ -1,5 +1,25 @@
 @NonCPS
-createIssues(){
+createIssues(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projectName = '"'+resultJson.name+'"'
+def length = 3
+def projLength = resultJson.name.size()
+ if(projLength>=3){
+  key=resultJson.name.substring(0, Math.min(projLength, length)).toUpperCase();
+ }
+ else {
+   def appendStr = "";
+   int currentLength=length;
+   int len = currentLength%projLength;
+   int repeat = currentLength/projLength;
+   for (int i=0;i<repeat;i++) {
+    appendStr = appendStr + resultJson.name;
+   }
+   appendStr=appendStr+resultJson.name.substring(0, Math.min(projectName.length(), len));
+   key=appendStr.toUpperCase();
+ }
+ def projKey = '"'+key+'"'
 //def projKey = '"'+key+'"'
  
  httpRequest authentication: 'jira_password', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], [maskValue: false, name: 'Accept', value: 'application/json']], httpMode: 'POST', requestBody: '''{
@@ -35,5 +55,6 @@ createIssues(){
 }
 
 def call(){
- createIssues()
+def request = libraryResource 'data.json'
+ createIssues(request)
 }
